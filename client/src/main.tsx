5c9ede64392c71aa3b39c62a14c1ab6f85716a5f
Pravisha Sadhwani <sadhwanipravisha@gmail.com>
2026-03-18 22:12:20 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// 🔥 Theme apply on load
+const savedTheme = localStorage.getItem("theme") || "light";
+
+if (savedTheme === "dark") {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
